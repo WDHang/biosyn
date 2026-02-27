7d7cf954e7673b52a4e7dcc4e7dad4f879e34819
WDHang <1782723027@qq.com>
2026-02-27 15:06:57 +0800

    Fix: Remove duplicate incomplete SUBSTRATE_DB definition

--- app_streamlit.py
+++ app_streamlit.py
@@ -51,14 +51,7 @@ SUBSTRATE_DB = {
     'Talose': {'mw': 180.16, 'carbon': 6, 'c_type': 'C6'},
     'Psicose': {'mw': 180.16, 'carbon': 6, 'c_type': 'C6'},
 }
-SUBSTRATE_DB = {
-    'GALD': {'mw': 60.05, 'carbon': 2, 'c_type': 'C2'},
-    'Erythrose': {'mw': 120.10, 'carbon': 4, 'c_type': 'C4'},
-    'Threose': {'mw': 120.10, 'carbon': 4, 'c_type': 'C4'},
-    'Erythrulose': {'mw': 120.10, 'carbon': 4, 'c_type': 'C4'},
-    'Glucose': {'mw': 180.16, 'carbon': 6, 'c_type': 'C6'},
-    'Fructose': {'mw': 180.16, 'carbon': 6, 'c_type': 'C6'},
-}
+
 
 def get_substrate_list():
     return list(SUBSTRATE_DB.keys())
